31b0711d75fa1c3a5228e8f60177c535424ed9e5
JuanCDiazL <jdiazlop@unal.edu.co>
2025-09-08 17:24:08 -0500

    Update Letra T

---
+++ Letra_T.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+col_tar = (0, 0, 255)
+col_bg = (0, 0, 0)
+
+letra_r = np.array([
+    [col_bg[0], col_bg[0], col_bg[0], col_bg[0], col_bg[0], col_bg[0], col_bg[0]],
+    [col_bg[0], col_tar[0], col_tar[0], col_tar[0], col_tar[0], col_tar[0], col_bg[0]],
+    [col_bg[0], col_bg[0], col_bg[0], col_tar[0], col_bg[0], col_bg[0], col_bg[0]],
+    [col_bg[0], col_bg[0], col_bg[0], col_tar[0], col_bg[0], col_bg[0], col_bg[0]],
+    [col_bg[0], col_bg[0], col_bg[0], col_tar[0], col_bg[0], col_bg[0], col_bg[0]],
+    [col_bg[0], col_bg[0], col_bg[0], col_tar[0], col_bg[0], col_bg[0], col_bg[0]],
+    [col_bg[0], col_bg[0], col_bg[0], col_tar[0], col_bg[0], col_bg[0], col_bg[0]],
+], dtype=np.uint8)
+
+letra_g = np.array([[col_bg[1], col_bg[1], col_bg[1], col_bg[1], col_bg[1], col_bg[1], col_bg[1]],
+                    [col_bg[1], col_tar[1], col_tar[1], col_tar[1], col_tar[1], col_tar[1], col_bg[1]],
+                    [col_bg[1], col_bg[1], col_bg[1], col_tar[1], col_bg[1], col_bg[1], col_bg[1]],
+                    [col_bg[1], col_bg[1], col_bg[1], col_tar[1], col_bg[1], col_bg[1], col_bg[1]],
+                    [col_bg[1], col_bg[1], col_bg[1], col_tar[1], col_bg[1], col_bg[1], col_bg[1]],
+                    [col_bg[1], col_bg[1], col_bg[1], col_tar[1], col_bg[1], col_bg[1], col_bg[1]],
+                    [col_bg[1], col_bg[1], col_bg[1], col_tar[1], col_bg[1], col_bg[1], col_bg[1]],
+                    ], dtype=np.uint8)
+
+letra_b = np.array([[col_bg[2], col_bg[2], col_bg[2], col_bg[2], col_bg[2], col_bg[2], col_bg[2]],
+                    [col_bg[2], col_tar[2], col_tar[2], col_tar[2], col_tar[2], col_tar[2], col_bg[2]],
+                    [col_bg[2], col_bg[2], col_bg[2], col_tar[2], col_bg[2], col_bg[2], col_bg[2]],
+                    [col_bg[2], col_bg[2], col_bg[2], col_tar[2], col_bg[2], col_bg[2], col_bg[2]],
+                    [col_bg[2], col_bg[2], col_bg[2], col_tar[2], col_bg[2], col_bg[2], col_bg[2]],
+                    [col_bg[2], col_bg[2], col_bg[2], col_tar[2], col_bg[2], col_bg[2], col_bg[2]],
+                    [col_bg[2], col_bg[2], col_bg[2], col_tar[2], col_bg[2], col_bg[2], col_bg[2]],
+                    ], dtype=np.uint8)
+
+letra = np.stack((letra_r, letra_g, letra_b), axis=2)
+
+
+plt.imshow(letra)
+plt.show()
