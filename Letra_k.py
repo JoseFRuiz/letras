2c30dc6a94a6760657c18326a4d4eaf048e65d51
JoseFRuiz <jfruizmu@gmail.com>
2025-09-10 15:25:24 -0500

    Update letters: K and M

--- Letra_k.py
+++ Letra_k.py
@@ -1,47 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-col_tar = (255, 0, 0)    
-col_bg  = (255, 255, 255) 
+col_tar = (0, 0, 0)
+col_bg = (255, 255, 255)
 
+letra_r = np.array([[col_tar[0], col_bg[0], col_bg[0], col_bg[0], col_tar[0]],
+                    [col_tar[0], col_bg[0], col_bg[0], col_tar[0], col_bg[0]],
+                    [col_tar[0], col_bg[0], col_tar[0], col_bg[0], col_bg[0]],
+                    [col_tar[0], col_tar[0], col_bg[0], col_bg[0], col_bg[0]],
+                    [col_tar[0], col_bg[0], col_tar[0], col_bg[0], col_bg[0]],
+                    [col_tar[0], col_bg[0], col_bg[0], col_tar[0], col_bg[0]],
+                    [col_tar[0], col_bg[0], col_bg[0], col_bg[0], col_tar[0]]], dtype=np.uint8)
 
-letrak_r = np.array([
-    [col_tar[0], col_bg[0], col_bg[0], col_tar[0], col_bg[0], col_bg[0], col_bg[0]],
-    [col_tar[0], col_bg[0], col_tar[0], col_bg[0], col_bg[0], col_bg[0], col_bg[0]],
-    [col_tar[0], col_tar[0], col_bg[0], col_bg[0], col_bg[0], col_bg[0], col_bg[0]],
-    [col_tar[0], col_tar[0], col_bg[0], col_bg[0], col_bg[0], col_bg[0], col_bg[0]],
-    [col_tar[0], col_bg[0], col_tar[0], col_bg[0], col_bg[0], col_bg[0], col_bg[0]],
-    [col_tar[0], col_bg[0], col_bg[0], col_tar[0], col_bg[0], col_bg[0], col_bg[0]],
-    [col_tar[0], col_bg[0], col_bg[0], col_bg[0], col_tar[0], col_bg[0], col_bg[0]]
-], dtype=np.uint8)
+letra_g = np.array([[col_tar[1], col_bg[1], col_bg[1], col_bg[1], col_tar[1]],
+                    [col_tar[1], col_bg[1], col_bg[1], col_tar[1], col_bg[1]],
+                    [col_tar[1], col_bg[1], col_tar[1], col_bg[1], col_bg[1]],
+                    [col_tar[1], col_tar[1], col_bg[1], col_bg[1], col_bg[1]],
+                    [col_tar[1], col_bg[1], col_tar[1], col_bg[1], col_bg[1]],
+                    [col_tar[1], col_bg[1], col_bg[1], col_tar[1], col_bg[1]],
+                    [col_tar[1], col_bg[1], col_bg[1], col_bg[1], col_tar[1]]], dtype=np.uint8)
 
+letra_b = np.array([[col_tar[2], col_bg[2], col_bg[2], col_bg[2], col_tar[2]],
+                    [col_tar[2], col_bg[2], col_bg[2], col_tar[2], col_bg[2]],
+                    [col_tar[2], col_bg[2], col_tar[2], col_bg[2], col_bg[2]],
+                    [col_tar[2], col_tar[2], col_bg[2], col_bg[2], col_bg[2]],
+                    [col_tar[2], col_bg[2], col_tar[2], col_bg[2], col_bg[2]],
+                    [col_tar[2], col_bg[2], col_bg[2], col_tar[2], col_bg[2]],
+                    [col_tar[2], col_bg[2], col_bg[2], col_bg[2], col_tar[2]]], dtype=np.uint8)
 
-letrak_g = np.array([
-    [col_tar[1], col_bg[1], col_bg[1], col_tar[1], col_bg[1], col_bg[1], col_bg[1]],
-    [col_tar[1], col_bg[1], col_tar[1], col_bg[1], col_bg[1], col_bg[1], col_bg[1]],
-    [col_tar[1], col_tar[1], col_bg[1], col_bg[1], col_bg[1], col_bg[1], col_bg[1]],
-    [col_tar[1], col_tar[1], col_bg[1], col_bg[1], col_bg[1], col_bg[1], col_bg[1]],
-    [col_tar[1], col_bg[1], col_tar[1], col_bg[1], col_bg[1], col_bg[1], col_bg[1]],
-    [col_tar[1], col_bg[1], col_bg[1], col_tar[1], col_bg[1], col_bg[1], col_bg[1]],
-    [col_tar[1], col_bg[1], col_bg[1], col_bg[1], col_tar[1], col_bg[1], col_bg[1]]
-], dtype=np.uint8)
+letra = np.stack((letra_r, letra_g, letra_b), axis=2)
 
-
-letrak_b = np.array([
-    [col_tar[2], col_bg[2], col_bg[2], col_tar[2], col_bg[2], col_bg[2], col_bg[2]],
-    [col_tar[2], col_bg[2], col_tar[2], col_bg[2], col_bg[2], col_bg[2], col_bg[2]],
-    [col_tar[2], col_tar[2], col_bg[2], col_bg[2], col_bg[2], col_bg[2], col_bg[2]],
-    [col_tar[2], col_tar[2], col_bg[2], col_bg[2], col_bg[2], col_bg[2], col_bg[2]],
-    [col_tar[2], col_bg[2], col_tar[2], col_bg[2], col_bg[2], col_bg[2], col_bg[2]],
-    [col_tar[2], col_bg[2], col_bg[2], col_tar[2], col_bg[2], col_bg[2], col_bg[2]],
-    [col_tar[2], col_bg[2], col_bg[2], col_bg[2], col_tar[2], col_bg[2], col_bg[2]]
-], dtype=np.uint8)
-
-
-letrak = np.stack((letrak_r, letrak_g, letrak_b), axis=2)
-
-plt.imshow(letrak)
+plt.imshow(letra)
 plt.show()
-
-
-
