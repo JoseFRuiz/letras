a5e85cacb25a0ab6fc3818d858b2eded24e27093
JESUS-LASTRA <jlastrar@unal.edu.co>
2025-09-09 20:32:57 -0500

    tarea

---
+++ Letra F mayus.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+col_tar = (0, 0, 255) 
+col_bg = (0, 128, 0)   
+
+letra_r_F = np.array([
+    [col_tar[0], col_tar[0], col_tar[0], col_tar[0], col_bg[0]],
+    [col_tar[0], col_bg[0], col_bg[0], col_bg[0], col_bg[0]],
+    [col_tar[0], col_bg[0], col_bg[0], col_bg[0], col_bg[0]],
+    [col_tar[0], col_tar[0], col_tar[0], col_tar[0], col_bg[0]],
+    [col_tar[0], col_bg[0], col_bg[0], col_bg[0], col_bg[0]],
+    [col_tar[0], col_bg[0], col_bg[0], col_bg[0], col_bg[0]],
+    [col_tar[0], col_bg[0], col_bg[0], col_bg[0], col_bg[0]]
+], dtype=np.uint8)
+
+letra_g_F = np.array([
+    [col_tar[1], col_tar[1], col_tar[1], col_tar[1], col_bg[1]],
+    [col_tar[1], col_bg[1], col_bg[1], col_bg[1], col_bg[1]],
+    [col_tar[1], col_bg[1], col_bg[1], col_bg[1], col_bg[1]],
+    [col_tar[1], col_tar[1], col_tar[1], col_tar[1], col_bg[1]],
+    [col_tar[1], col_bg[1], col_bg[1], col_bg[1], col_bg[1]],
+    [col_tar[1], col_bg[1], col_bg[1], col_bg[1], col_bg[1]],
+    [col_tar[1], col_bg[1], col_bg[1], col_bg[1], col_bg[1]]
+], dtype=np.uint8)
+
+letra_b_F = np.array([
+    [col_tar[2], col_tar[2], col_tar[2], col_tar[2], col_bg[2]],
+    [col_tar[2], col_bg[2], col_bg[2], col_bg[2], col_bg[2]],
+    [col_tar[2], col_bg[2], col_bg[2], col_bg[2], col_bg[2]],
+    [col_tar[2], col_tar[2], col_tar[2], col_tar[2], col_bg[2]],
+    [col_tar[2], col_bg[2], col_bg[2], col_bg[2], col_bg[2]],
+    [col_tar[2], col_bg[2], col_bg[2], col_bg[2], col_bg[2]],
+    [col_tar[2], col_bg[2], col_bg[2], col_bg[2], col_bg[2]]
+], dtype=np.uint8)
+
+letra = np.stack((letra_r_F, letra_g_F, letra_b_F), axis=2)
+
+
+plt.imshow(letra)
+plt.show()
